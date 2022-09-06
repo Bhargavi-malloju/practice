8d811821651256fc9e65b3f3f1ec557df37f7bc3
Bhargavi.M <78975273+Bhargavi-malloju@users.noreply.github.com>
2022-09-06 09:56:08 +0530

    Update timer.py

--- timer.py
+++ timer.py
@@ -13,14 +13,3 @@ print("give in seconds to countdown!")
 seconds=int(input())
 countdown(seconds)
 
-import time
-def countdown(t):
-    while t>0:
-        print(t)
-        t-=1
-        time.sleep(1)
-    print("finished")
-
-print("how mny seconds to countdown?")
-seconds=int(input())
-countdown(seconds)
